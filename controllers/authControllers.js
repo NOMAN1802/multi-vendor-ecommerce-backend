3730e9a2f6538875b8221fa3d4b30b289eb21516
NOMAN1802 <mustakimnoman82@gmail.com>
2024-03-26 15:54:31 +0600

    server set up database and auth route

---
+++ controllers/authControllers.js
@@ -0,0 +1,15 @@
+const adminModel = require('../models/adminModel')
+class authControllers {
+ 
+    admin_login = async(req, res)=>{
+        const {email, password} = req.body
+        try{
+          const admin = await adminModel.findOne({email}).select('+password')
+          console.log(admin);
+        }catch(error) {
+
+        }
+    }
+}
+
+module.exports = new authControllers()
